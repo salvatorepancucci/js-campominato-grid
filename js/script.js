d6f572456d75b8337b8293ab8236852e8ed10b5b
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-05 17:32:15 +0200

    Main grid function added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 // Seleziona il bottone e il contenitore della griglia dal DOM
 const generateGridButton = document.getElementById('generateGrid');
 const gridContainer = document.getElementById('gridContainer');
+
+// Funzione per generare la griglia
+function generateGrid() {
+
+}
