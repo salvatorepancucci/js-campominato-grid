e6afbff1ecec0054321aeea62dfaa9d78e9f737d
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-05 17:31:40 +0200

    Get grid DOM's buttons

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// Seleziona il bottone e il contenitore della griglia dal DOM
+const generateGridButton = document.getElementById('generateGrid');
+const gridContainer = document.getElementById('gridContainer');
